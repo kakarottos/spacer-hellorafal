a85c73f20dd0c839379a6f0a2749e48b7a4487f9
kakarottos <kakarotto162@gmail.com>
2018-12-25 12:29:57 +0100

    GitDes

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app">
+    <div class="nav">
+      <router-link to="/">Spacer</router-link>
+      <router-link to="/About">About</router-link>
+    </div>
+
+    <router-view />
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="Home">
+    <h1>Spacer</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+};
+
+</script>
